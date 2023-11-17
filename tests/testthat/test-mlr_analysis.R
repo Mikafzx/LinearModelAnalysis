9bd0bdda16e5fbaaf8527b9303357cb843c1a436
zhuoxinf <zhoxinf@umich.edu>
2023-11-17 09:34:44 -0500

    modify

--- tests/testthat/test-mlr_analysis.R
+++ tests/testthat/test-mlr_analysis.R
@@ -1,18 +1,18 @@
 test_that("mlr_analysis works", {
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl, data = mtcars)$coeff_table),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl, input_data = mtcars)$coeff_table),
                as.numeric(lm(mpg ~ hp + cyl, mtcars)$coeff_table))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl, data = mtcars)$df),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl, input_data = mtcars)$df),
                as.numeric(summary(lm(mpg ~ cyl + wt, mtcars))$DF))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt, data = mtcars)$coeff_table),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt, input_data = mtcars)$coeff_table),
                as.numeric(lm(mpg ~ hp + cyl + wt, mtcars)$coeff_table))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt, data = mtcars)$df),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt, input_data = mtcars)$df),
                as.numeric(lm(mpg ~ hp + cyl + wt, mtcars)$DF))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec, data = mtcars)$coeff_table),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec, input_data = mtcars)$coeff_table),
                as.numeric(lm(mpg ~ hp + cyl + wt + qsec, mtcars)$coeff_table))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec, data = mtcars)$df),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec, input_data = mtcars)$df),
                as.numeric(summary(lm(mpg ~ hp + cyl + wt + qsec, mtcars))$DF))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec + cyl * wt, data = mtcars)$coeff_table),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec + cyl * wt, input_data = mtcars)$coeff_table),
                as.numeric(lm(mpg ~ hp + cyl + wt + qsec + cyl * wt, mtcars)$coeff_table))
-  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec + cyl * wt, data = mtcars)$df),
+  expect_equal(as.numeric(mlr_analysis(formula = mpg ~ hp + cyl + wt + qsec + cyl * wt, input_data = mtcars)$df),
                as.numeric(summary(lm(mpg ~ hp + cyl + wt + qsec + cyl * wt, mtcars))$DF))
 })
